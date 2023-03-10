f1737a0c4aed146116e8bd139e02cb7aebe831b1
Godwin Waswa <godwnwaswa@gmail.com>
2023-03-10 16:34:24 +0300

    Documented the core/base routes

--- platform/core/base/routes/web.php
+++ platform/core/base/routes/web.php
@@ -2,24 +2,29 @@
 
 use Botble\Base\Http\Controllers\SystemController;
 
+
 Route::group(['namespace' => 'Botble\Base\Http\Controllers', 'middleware' => ['web', 'core']], function () {
+    //a.prefixed e.g --admin/**/
     Route::group(['prefix' => BaseHelper::getAdminPrefix(), 'middleware' => 'auth'], function () {
+        //i.prefixed --admin/system/info/**/
         Route::group(['prefix' => 'system/info'], function () {
+            //--admin/system/info
             Route::match(['GET', 'POST'], '', [
                 'as'         => 'system.info',
                 'uses'       => 'SystemController@getInfo',
                 'permission' => ACL_ROLE_SUPER_USER,
             ]);
         });
-
+        //ii.prefixed --admin/system/cache/**/
         Route::group(['prefix' => 'system/cache'], function () {
-
+            //--admin/system/cache
             Route::get('', [
                 'as'         => 'system.cache',
                 'uses'       => 'SystemController@getCacheManagement',
                 'permission' => ACL_ROLE_SUPER_USER,
             ]);
 
+            //--admin/system/cache/clear
             Route::post('clear', [
                 'as'         => 'system.cache.clear',
                 'uses'       => 'SystemController@postClearCache',
@@ -28,6 +33,7 @@ Route::group(['namespace' => 'Botble\Base\Http\Controllers', 'middleware' => ['w
             ]);
         });
 
+        //iii.prefixed --admin/membership/authorize
         Route::post('membership/authorize', [
             'as'         => 'membership.authorize',
             'uses'       => 'SystemController@authorize',
@@ -35,5 +41,6 @@ Route::group(['namespace' => 'Botble\Base\Http\Controllers', 'middleware' => ['w
         ]);
     });
 
+     //b. --settings-language/{alias}
     Route::get('settings-language/{alias}', [SystemController::class, 'getLanguage'])->name('settings.language');
 });
