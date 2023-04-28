fa0101d3481a70785f686bb05fd1f18211c1c0bd
Godwin Waswa <godwnwaswa@gmail.com>
2023-04-28 22:50:16 +0300

    updated & added a logout test

--- tests/Feature/ACLTest.php
+++ tests/Feature/ACLTest.php
@@ -5,19 +5,33 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use Facades\RealDriss\Base\Helpers\BaseHelper;
 
 class ACLTest extends TestCase
 {
     /**
-     * The admin/post route requires email && password
-     * we're posting an empty submission to trigger an error.
+     * Test if posting an empty submission to admin/login trigger an error.
      *
      * @return void
      */
     public function test_admin_login_with_empty_submission()
     {
-        $response = $this->post(BaseHelper::getAdminPrefix().'/login', []);
+        $response = $this->post(route('access.login'), []);
         $response->assertSessionHasErrors();
+    }
+
+
+    /**
+     * Test if on logout the user is redirected to the login page
+     * with a logout success message.
+     */
+    public function test_admin_logout_redirects_to_login()
+    {
+        $response = $this->post(route('access.login'), 
+            [
+                'username' => 'godwin@realdriss.com', 
+                'password' => 'godwin@18',
+            ]);
+        $response = $this->get(route('access.logout'));
+        $response->assertRedirect(route('access.login'));
     }
 }
