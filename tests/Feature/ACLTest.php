b2054af809aff5e06b887f00d1ad73377f71bad3
Godwin Waswa <godwnwaswa@gmail.com>
2023-04-29 10:14:04 +0300

    updated acltest

--- tests/Feature/ACLTest.php
+++ tests/Feature/ACLTest.php
@@ -15,10 +15,9 @@ class ACLTest extends TestCase
      */
     public function test_admin_login_with_empty_submission()
     {
-        $response = $this->post(route('access.login'), []);
+        $response = $this->post(route('access.login.post'), []);
         $response->assertSessionHasErrors();
     }
-
 
     /**
      * Test if on logout the user is redirected to the login page
@@ -26,12 +25,27 @@ class ACLTest extends TestCase
      */
     public function test_admin_logout_redirects_to_login()
     {
-        $response = $this->post(route('access.login'), 
+        $response = $this->post(route('access.login.post'), 
             [
                 'username' => 'godwin@realdriss.com', 
                 'password' => 'godwin@18',
             ]);
         $response = $this->get(route('access.logout'));
+        $response->assertRedirect(route('access.login'));
+    }
+
+    /**
+     * Test if a user with wrong credentials is redirected back to the 
+     * login page.
+     */
+    public function test_admin_user_wrong_credentials_redirects_to_login()
+    {
+        $response = $this->post(route('access.login.post'), 
+            [
+                'username' => 'godwin@realdriss.com', 
+                'password' => '0000',
+            ]);
+        
         $response->assertRedirect(route('access.login'));
     }
 }
