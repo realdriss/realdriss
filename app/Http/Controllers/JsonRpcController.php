ee6d182f49c1999392370c9bb8cf5144b6019f61
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-05 21:56:41 +0300

    update

--- app/Http/Controllers/JsonRpcController.php
+++ app/Http/Controllers/JsonRpcController.php
@@ -1,38 +1,23 @@
 <?php
 
-namespace App\Http\Controllers;
-
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class JsonRpcController extends Controller
 {
     public function handle(Request $request)
     {
-        $requestData = json_decode($request->getContent(), true);
-
-        $method = $requestData['method'];
-        $params = $requestData['params'];
-        $id = $requestData['id'];
-
-        switch ($method) {
-            case 'add':
-                $result = array_sum($params);
-                $response = [
-                    'jsonrpc' => '2.0',
-                    'result' => $result,
-                    'id' => $id,
-                ];
-                return response()->json($response);
-            default:
-                $error = [
-                    'jsonrpc' => '2.0',
-                    'error' => [
-                        'code' => -32601,
-                        'message' => 'Method not found',
-                    ],
-                    'id' => $id,
-                ];
-                return response()->json($error);
-        }
+        $client = new Client([
+            'base_uri' => 'http://localhost:8000/rpc',
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+        
+        $response = $client->post('', [
+            'json' => $request->all(),
+        ]);
+        
+        return response()->json(json_decode($response->getBody(), true));
     }
 }
