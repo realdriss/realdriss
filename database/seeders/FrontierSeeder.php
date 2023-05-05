7ab9c39907b29023d843325be466207f82116088
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-05 09:38:31 +0300

    added a seeder

---
+++ database/seeders/FrontierSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use RealDriss\Base\Supports\BaseSeeder;
+use RealDriss\Blog\Models\Category;
+use RealDriss\Blog\Models\Post;
+
+
+class FrontierSeeder extends BaseSeeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->truncateKeyTables();
+        $this->activateAllPlugins();
+    }
+}
